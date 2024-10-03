50f30cb291590f827dbdeec5da165f8a65e40ade
nicholas sadd <jns014@bucknell.edu>
2024-10-03 10:30:25 -0400

    ffmpeg init commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,24 @@ import aws from "astro-sst"
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	adapter: aws()
+  output: "server",
+  adapter: aws(),
+  integrations: [tailwind(), react()],
+  vite: {
+    optimizeDeps: {
+      exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
+    },
+  },
+  server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
 });
